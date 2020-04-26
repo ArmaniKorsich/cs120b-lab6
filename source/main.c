5e6e74b775bfceb3c2735460d89830040c09db91
ArmaniKorsich <33210973+ArmaniKorsich@users.noreply.github.com>
2020-04-26 16:27:42 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -8,16 +8,62 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long_avr_timer_cntcurr = 0;
+
+void TimerOn() {
+    
+    TCCR1B = 0x0b;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    TCNT1 = 0;
+    
+    _avr_timer_cntcurr = _avr_timer_M;
+    Sreg |= 0x80;
+}
+
+void Timerff() {
+    TCCR1B = 0x00;
+}
+
+void TimerISR() {
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    _avr_timer_cntcurr--;
+    if(_avr_timer_cntcurr == 0) {
+        TimerISR();
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+
+void TimerSet(unsigned long M) {
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+    
+    
+void main() {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    TimerSet(1000);
+    TimerOn();
 
     /* Insert your solution below */
     while (1) {
-
+        while (!TimerFlag);
+        TimerFlag = 0;
     }
     return 1;
 }
